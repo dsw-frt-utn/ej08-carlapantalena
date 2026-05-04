16100e53d50c2ea313ff5b9a185deb44c229fcc7
Carla Pantalena <Carla.Pantalena@alu.frt.utn.edu.ar>
2026-05-03 23:35:17 -0300

    Problema N4 resuelto

--- Dsw2026Ej8/Problema3.cs
+++ Dsw2026Ej8/Problema3.cs
@@ -8,19 +8,12 @@ namespace Dsw2026Ej8
     {
         public string CompararCopias(int originalValue, Product product)
         {
-            // 1. Copia por valor
-            int copiaValor = originalValue;
-
-            // 2. Incrementar solo la copia
+            int copiaValor = originalValue; 
             copiaValor++;
-
-            // 3. Copia de referencia
             Product copiaProducto = product;
 
-            // 4. Modificar el producto
             copiaProducto.SetDescription("Producto Modificado");
 
-            // 5. Retornar resultado
             return $"{originalValue}-{copiaValor}-{product.GetDescription()}";
         }
     }
